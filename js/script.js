aaf83153b7ea8dc252c945b5f0d3ad08eb193bae
Aleks <i.aleks@tut.by>
2024-03-14 22:50:11 +0300

    classList

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const btns = document.querySelectorAll('button');
+/*
+//проверка наличия классов
+console.log(btns[0].classList.length);
+//получение класса
+console.log(btns[0].classList.item(0));
+//добавление класса
+btns[0].classList.add('red');
+//удаление класса
+btns[0].classList.remove('blue');
+//присваение(удаление) класса
+btns[0].classList.toggle('blue');
+//проверка наличия класса
+if (btns[1].classList.contains('red')) {
+   console.log('red');
+}
+*/
+/*
+btns[0].addEventListener('click', () => {
+   btns[1].classList.toggle('red');
+})
+*/
+/*
+const wraper = document.querySelector('.btn-block');
+
+wraper.addEventListener('click', (event) => {
+   if (event.target && event.target.tagName == 'BUTTON') {
+      console.log('red');
+   }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wraper.append(btn);
+*/
