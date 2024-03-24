3ac6084bd15d388ebc71a3fe7b8e93a68f4a554e
Aleks <i.aleks@tut.by>
2024-03-24 18:39:40 +0300

    class

--- js/script.js
+++ js/script.js
@@ -192,3 +192,32 @@ console.log(double(3));
 // !!!! у стрелочных функций нет контекста вызова (берет у родителя)
 */
 
+//==================Классы=============================
+
+class Rectangle {
+   constructor(height, width) {
+      this.height = height;
+      this.width = width;
+   }
+   calcArea() {
+      return this.height * this.width;
+   }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+   constructor(height, width, text, bgColor) {
+      super(height, width);
+      this.text = text;
+      this.bgColor = bgColor;
+   }
+
+   showMyProps() {
+      console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+   }
+}
+
+const square = new Rectangle(10, 10);
+console.log(square.calcArea());
+const div = new ColoredRectangleWithText(10, 15, 'rect', 'red');
+div.showMyProps();
+console.log(div.calcArea());
